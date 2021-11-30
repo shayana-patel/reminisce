8e7a3722624535fc8ac38580e1a7fff3e4d8d663
shayana-patel <shayana_patel@hotmail.com>
2021-11-30 21:15:12 +1300

    more styling of header/nav bar

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import Nav from './Nav'
 
 const Header = () => {
   return (
     <header className='header'>
-      <Link to='/'><h1 className='header-title'>-- Reminisce --</h1></Link>
-      <br/>
       <Nav />
     </header>
   )
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className='nav'>
-      <Link to="/concerts"><h2 className='concerts-nav-link'>Concerts</h2></Link>
-      <Link to='/travels'><h2 className='travels-nav-link'>Travels</h2></Link>
+      <Link to='/' className='app-title'><h1>-- Reminisce --</h1></Link>
+      <Link to='/concerts' className='concerts-nav-link'><h2>Concerts</h2></Link>
+      <Link to='/travels' className='travels-nav-link'><h2>Travels</h2></Link>
     </nav>
   )
 }
