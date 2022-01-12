21d3c2328840dde3d4d172a2374681ae8bb5931e
shayana-patel <shayana_patel@hotmail.com>
2022-01-13 10:47:32 +1300

    update production value in clientside config.js file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+if (!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
   const envConfig = require('dotenv').config()
   if (envConfig.error) throw envConfig.error
 }
@@ -8,5 +8,6 @@ const server = require('./server')
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('Listening on port', PORT)
 })
